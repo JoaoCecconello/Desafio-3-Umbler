a3ea85f1af1abbf076b566240184a35046938baf
JoaoCecconello <jvcecconello@gmail.com>
2020-11-11 19:01:51 -0300

    MongoDB

--- routes/alunos.js
+++ routes/alunos.js
@@ -4,22 +4,17 @@ const router = express.Router();
 const {MongoClient} = require('mongodb');
 
 router.get('/alunos', async function(req,res){
-    alunos = await DBConnection();
-    res.send(`${alunos}`)
-});
-
-async function DBConnection(){
-    const uri = "mongodb+srv://nephiladb:desafio3DB@mongo_desafio-3:27017/test?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://mongo_desafio-3:27017/?w=majority";
     const client = new MongoClient(uri);
-
     try {
         await client.connect();
-        return alunos = client.db("desafio-3").collection("alunos").find();
+        const alunos = await client.db("desafio-3").collection("alunos").find({});
+        res.send(alunos);
     } catch (e) {
         console.error(e);
     } finally {
         await client.close();
     }
-}
+});
 
 module.exports = router;
