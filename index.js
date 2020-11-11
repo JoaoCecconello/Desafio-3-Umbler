1b1986d01414fe981e1c76451c8e7f317eb1c2c9
JoaoCecconello <jvcecconello@gmail.com>
2020-11-11 14:50:35 -0300

    Routes & HHTPS

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express();
+
+app.use((req, res, next) => {
+    if ((req.headers["x-forwarded-proto"] || "").endsWith("http"))
+        res.redirect(`https://${req.headers.host}${req.url}`);
+    else
+        next();
+});
+
+app.get('/' , function(req,res){ res.send('Hello World') })
+
+app.get('/hora' , function(req,res){ res.send('Hora') })
+
+app.get('/alunos' , function(req,res){ res.send('Alunos') })
