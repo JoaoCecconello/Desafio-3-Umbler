89dc36b45c6beff4f6d4e31350ff69bce290b206
JoaoCecconello <jvcecconello@gmail.com>
2020-11-11 21:24:51 -0300

    MongoDB

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/' , function(req,res){ res.send('Hello World') })
 
 
 async function DBConnection(){
-    const url = "mongodb://nephiladb:desafio3DB@geonosis.mongodb.umbler.com:35888/?authSource=admin";
+    const url = "mongodb://mongo_desafio-3:27017/?authSource=admin";
     const client = new MongoClient(url, { useUnifiedTopology: true });
     let allResults = {};
     try {
